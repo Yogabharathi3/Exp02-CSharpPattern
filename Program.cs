030cfd7f29d329f8f5ca53fab5dbd05e1e637a9e
Yogabharathi3 <yogabharathi76@gmail.com>
2024-02-24 10:59:08 +0530

    first commit

--- Program.cs
+++ Program.cs
@@ -2,8 +2,26 @@
 using System;
 public class Pattern
 {
-    public static void Main()
+    class Example
     {
-        // type the code here
+        public static void Main()
+        {
+            int rows = 5, c=1, s, i, j;
+            Console.WriteLine("Pascal's triangle");
+            for (i = 0; i < rows; i++)
+            {
+                for (s = 1; s <= rows - i; s++)
+                    Console.Write(" ");
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        c = 1;
+                    else
+                        c = c * (i - j + 1) / j;
+                    Console.Write(c + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
